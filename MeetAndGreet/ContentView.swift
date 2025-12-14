fa7389c0f055ca98a96546e9f97c87f13f630a5c
nkm119315 <nakamo326@icloud.com>
2025-12-14 14:24:42 +0900

    Initial Commit

---
+++ MeetAndGreet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeetAndGreet
+//
+//  Created by 中本竣介 on 2025/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetAndGreet/MeetAndGreetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetAndGreetApp.swift
+//  MeetAndGreet
+//
+//  Created by 中本竣介 on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct MeetAndGreetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
